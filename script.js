f52c8c09d25a0854cf0b101ba2c6cdb773200b2e
LUIZ GUSTAVO LORENZON BARBOSA <57134279863@info.local>
2024-05-23 10:25:49 -0300

    Feat: terminado carrosel

--- script.js
+++ script.js
@@ -3,40 +3,33 @@ let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.getElementById("icone-x")
 
 function abreFechaMenu() {
+       
+    if (menu.classList.contains("menu-fechado")){
+        
+        menu.classList.remove("menu-fechado")
 
+        iconeBarras.style.display = "none"
 
+        iconeX.style.display = "inline"
+    } else {
+        menu.classList.add("menu-fechado")
 
-    if (menu.classList.contains("menu-fechado")) {
+        iconeX.style.display = "none"
 
+        iconeBarras.style.display = "inline"
+    }
 
+}
+
+onresize = () => {
         menu.classList.remove("menu-fechado")
 
         iconeBarras.style.display = "none"
 
         iconeX.style.display = "inline"
 
-    } else {
-
-        menu.classList.add("menu-fechado")
-
-        iconeX.style.display = "none"
-
-        iconeBarras.style.display = "inline"
-
-    }
-
 }
-onresize = () => {
 
-    menu.classList.remove("menu-fechado")
-
-    iconeBarras.style.display = "none"
-
-    iconeX.style.display = "inline"
-
-
-
-}
 let banner = document.querySelector(".banner")
 
 let slides = [
@@ -48,11 +41,40 @@ let slides = [
 let slideAtual = 0
 
 banner.classList.add(slides[slideAtual])
+
 function mostrarProximoSlide() {
+
 
     banner.classList.remove(slides[slideAtual])
 
-    slideAtual++
+    if (slideAtual < 2) {
+        slideAtual++
+    } else {
+        slideAtual = 0
+    }
+   
 
     banner.classList.add(slides[slideAtual])
+}
+function mostrarSlideAnterior () {
+    banner.classList.remove(slides[slideAtual])
+
+    if (slideAtual > 0 ) {
+        slideAtual--
+    } else {
+        slideAtual = 2
+    }
+
+   
+
+    banner.classList.add(slides[slideAtual])
+}
+
+function selecionarSlide(indiceSlide) {
+
+    banner.classList.remove(slides[slideAtual])
+
+    slideAtual = indiceSlide
+
+banner.classList.add(slides[slideAtual])
 }
